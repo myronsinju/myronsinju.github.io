6dd140af763a4fe8e7b0c0c38a7407008dde3836
myronsinju <65003034+myronsinju@users.noreply.github.com>
2020-06-09 15:53:16 -0600

    website update
    update with java

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,4 +1,4 @@
-    var slideIndex = 1;
+  var slideIndex = 1;
 	  showSlides(slideIndex);
 	function plusSlides(n) {
 	  showSlides(slideIndex += n);
@@ -25,29 +25,29 @@
 
     // The following function adds a smooth scrolling to all links
 
-$(document).ready(function(){   
-    $("a").on('click', function(event) {
+// $(document).ready(function(){   
+//     $("a").on('click', function(event) {
         
-      // Make sure this.Link has a value before overriding default behavior
-      if (this.Link !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
+//       // Make sure this.Link has a value before overriding default behavior
+//       if (this.Link !== "") {
+//         // Prevent default anchor click behavior
+//         event.preventDefault();
   
-        // Store Link
-        var Link = this.Link;
+//         // Store Link
+//         var Link = this.Link;
   
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (2000) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(Link).offset().top
-        }, 2000, function(){
+//         // Using jQuery's animate() method to add smooth page scroll
+//         // The optional number (2000) specifies the number of milliseconds it takes to scroll to the specified area
+//         $('html, body').animate({
+//           scrollTop: $(Link).offset().top
+//         }, 2000, function(){
      
-          // Add Link (#) to URL when done scrolling (default click behavior)
-          window.location.Link = Link;
-        });
-      } // End if
-    });
-  });
+//           // Add Link (#) to URL when done scrolling (default click behavior)
+//           window.location.Link = Link;
+//         });
+//       } // End if
+//     });
+//   });
   
   // These functions open and close the contact form 
   function openForm() {
@@ -56,5 +56,4 @@ $(document).ready(function(){
   
   function closeForm() {
     document.getElementById("myForm").style.display = "none";
-    this.Close();
   }
